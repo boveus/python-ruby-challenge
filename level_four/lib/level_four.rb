0d6491096d0859049e2014b6cb37404b3eb62288
Brandon Stewart <boveus@gmail.com>
2018-05-05 15:08:18 -0400

    recursive solution is coming together for level 4

--- level_four/lib/level_four.rb
+++ level_four/lib/level_four.rb
@@ -19,12 +19,21 @@ class LevelFour
     "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=#{nothing_number}"
   end
 
+  def handle_nothing_response(nothing_number)
+    next_nothing_response = Nokogiri::HTML(open(nothing_url(nothing_number)))
+    next_nothing = next_nothing_response.text.split(' ').last
+    next_nothing_message = next_nothing_response.text.split(' ')[0..-2].join(' ')
+    return next_nothing, next_nothing_message
+  end
+
   def get_next(nothing_number = first_nothing)
-    next_nothing = Nokogiri::HTML(open(nothing_url(nothing_number))).text.split(' ').last
-    if next_nothing.to_i > 40000
+    next_nothing, next_nothing_message = handle_nothing_response(nothing_number)
+    if next_nothing_message == 'and the next nothing is'
+      get_next(next_nothing)
+    elsif next_nothing_message == 'Your hands are getting tired and the next nothing is'
       get_next(next_nothing)
     else
-      next_nothing
+      next_nothing_message + ' ' + next_nothing
     end
   end
 
