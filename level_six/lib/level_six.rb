c7032ebdc8ffd72a2e7da73721b86eafe849aa68
Brandon Stewart <boveus@gmail.com>
2018-05-05 23:06:16 -0400

    start on level six

---
+++ level_six/lib/level_six.rb
@@ -0,0 +1,34 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'zip'
+# http://www.pythonchallenge.com/pc/def/channel.html
+# http://www.pythonchallenge.com/pc/def/channel.zip
+
+
+class LevelSix
+  def initialize
+    open_next
+  end
+
+  def first_nothing
+    File.read('./level_six/data/channel/90052.txt').split(' ').last
+  end
+
+  def open_next(nothing = first_nothing)
+    next_nothing, next_message = parse_nothing(nothing)
+    puts "#{next_message} #{next_nothing}"
+    if next_message == 'Next nothing is'
+      open_next(next_nothing)
+    end
+  end
+
+  def parse_nothing(nothing)
+    next_nothing = File.read("./level_six/data/channel/#{nothing}.txt").split(' ').last
+    next_message = File.read("./level_six/data/channel/#{nothing}.txt").split(' ')[0..-2].join(' ')
+    puts File.open("./level_six/data/channel/#{nothing}.txt")
+    binding.pry
+    return next_nothing, next_message
+  end
+
+end
---
+++ level_six/lib/runner.rb
@@ -0,0 +1,3 @@
+require './level_six/lib/level_six'
+
+LevelSix.new
