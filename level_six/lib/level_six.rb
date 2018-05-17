61384bccb247deb015876602d389d14d82b600e7
Brandon Stewart <boveus@gmail.com>
2018-05-16 20:45:55 -0400

    complete level six

--- level_six/lib/level_six.rb
+++ level_six/lib/level_six.rb
@@ -1,13 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-require 'zip'
+require 'zip/zip'
+
+
 # http://www.pythonchallenge.com/pc/def/channel.html
 # http://www.pythonchallenge.com/pc/def/channel.zip
 
 
 class LevelSix
   def initialize
+    @entries = ''
     open_next
   end
 
@@ -15,24 +18,35 @@ class LevelSix
     File.read('./level_six/data/channel/90052.txt').split(' ').last
   end
 
-  def open_next(nothing = first_nothing)
-    next_nothing, next_message, comment = parse_nothing(nothing)
-    print "#{comment}"
-    if next_message == 'Next nothing is'
-      open_next(next_nothing)
+  def print_comment(nothing)
+    Zip::ZipFile.open("./level_six/data/channel.zip") do |zipfile|
+      @entries += zipfile.get_entry("#{nothing}.txt").comment
     end
   end
 
+  def open_next(nothing = first_nothing)
+    next_nothing, next_message = parse_nothing(nothing)
+    if next_message == 'Next nothing is'
+      print_comment(next_nothing)
+      open_next(next_nothing)
+    else
+      print @entries
+    end
+  end
+
+  # Wrong type of comments
+  # https://stackoverflow.com/questions/1588236/read-file-comments-from-a-zip-file-in-ruby
+  # blergh
   def parse_file_comment(file_name)
     result = `mdls "#{file_name}" | grep kMDItemFinderComment`
-    result.split('=').last
+    return result.split('=').last
   end
+
 
   def parse_nothing(nothing)
     file = "./level_six/data/channel/#{nothing}.txt"
     next_nothing = File.read("#{file}").split(' ').last
     next_message = File.read("#{file}").split(' ')[0..-2].join(' ')
-    comment = parse_file_comment(file)
-    return next_nothing, next_message, comment
+    return next_nothing, next_message
   end
 end
