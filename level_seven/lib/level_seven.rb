8ad03e4642a5c5a14c7e192d8dbfcb317337b99b
Brandon Stewart <boveus@gmail.com>
2018-05-16 20:53:34 -0400

    add level 7 notes

---
+++ level_seven/lib/level_seven.rb
@@ -0,0 +1,7 @@
+# http://www.pythonchallenge.com/pc/def/hockey.html
+# or
+# http://www.pythonchallenge.com/pc/def/oxygen.html
+# Something is embedded in the image..
+# Page title is 'smarty'
+# https://pythonhosted.org/pypng/png.html
+# 
